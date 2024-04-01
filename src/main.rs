bd573a5e5ffb0d99b335fdfaca40c438509e0f3a
KKKZOZ <kkkzoz@qq.com>
2024-04-01 15:18:25 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::Write;
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    stream.write_all(b"+PONG\r\n").unwrap();
 }
